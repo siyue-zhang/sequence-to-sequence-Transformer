454a51bdc350fe4b657b7dfe5b1bc89602754208
zhangsy1 <zhang_siyue@simtech.a-star.edu.sg>
2022-03-09 11:03:40 +0800

    sample data

---
+++ utils.py
@@ -0,0 +1,18 @@
+import os
+import random
+import torch
+import numpy as np
+import math
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def mkdirs(paths):
+    if isinstance(paths, list) and not isinstance(paths, str):
+        for path in paths:
+            mkdir(path)
+    else:
+        mkdir(paths)
+
+        
